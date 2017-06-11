9415c8323a492daa5c6fac1eb15ba6f0d153738a
jelldutr <jelle.dutry@student.arteveldehs.be>
2017-06-11 22:38:32 +0200

    Commit 11/06 2

---
+++ _site/js/main.js
@@ -0,0 +1,9 @@
+/*function resizeScreen()
+            {
+                var heights = window.height;
+                document.getElementsByClassName("container").style.height = 200;
+            };
+
+window.onload = resizeScreen();
+window.onresize = resizeScreen();
+*/
