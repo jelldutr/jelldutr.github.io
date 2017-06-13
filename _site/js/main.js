eeeec0baa93801c2450a79bb2545e2a92f2da730
jelldutr <jelle.dutry@student.arteveldehs.be>
2017-06-13 20:13:29 +0200

    Commit 13/06 1

--- _site/js/main.js
+++ _site/js/main.js
@@ -2,6 +2,12 @@
 var urlLink = 'jelldutr.github.io/'
 // Aanpassen als website online komt!
 
+
+/**
+ * Zorgt ervoor dat de gebruiker doorverwezen wordt naar de 
+ * pagina van het vervoermiddel dat hij aangegeven heeft.
+ * De locatie wordt meegegeven in de URL zoals de get-methode
+ */
 var locForm = document.getElementById("locForm")
 locForm.onsubmit = function redirect(e) {
 
@@ -10,3 +16,12 @@ locForm.onsubmit = function redirect(e) {
 
     window.location = x;
 }
+
+
+/**
+ * Google Maps Api map object aanmaken
+ */
+map = new google.maps.Map(document.getElementById('map'), {
+  center: {lat: -34.397, lng: 150.644},
+  zoom: 8
+});
